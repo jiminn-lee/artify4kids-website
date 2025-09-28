8013a36a1a3fa0fb3a92ff0e90361d13b95a9e07
Jimin Lee <jiminlee102@gmail.com>
2025-09-27 20:46:28 -0700

    update dependencies, globe setup

--- src/routes/chapters/+page.svelte
+++ src/routes/chapters/+page.svelte
@@ -4,6 +4,41 @@
 	import Map from '$lib/components/Map.svelte';
 	import chapters from '$lib/chapters.js';
 	import info from '$lib/info.js';
+	import { onMount } from 'svelte';
+
+		let globeElement;
+	onMount(async () => {
+		const Globe = (await import('globe.gl')).default;
+		const globe = new Globe(globeElement)
+			.globeImageUrl('/map3.jpg')
+			.pointOfView({ lat: 36, lng: -95 })
+			.globeOffset([0, 200])
+			// .htmlElementsData(impact.locations)
+			// .htmlElement((location) => {
+			// 	const globeMarkerContainer = document.createElement('div');
+			// 	globeMarkerContainer.style.transition = 'opacity 250ms';
+			// 	mount(GlobeMarker, {
+			// 		target: globeMarkerContainer,
+			// 		props: {
+			// 			location: location
+			// 		}
+			// 	});
+			// 	return globeMarkerContainer;
+			// })
+			// .htmlElementVisibilityModifier((el, isVisible) => {
+			// 	if (isVisible) {
+			// 		el.style.opacity = '1';
+			// 	} else {
+			// 		el.style.opacity = '0';
+			// 	}
+			// })
+			// .atmosphereColor('#4338ca');
+			.backgroundColor("rgba(0,0,0,0)")
+		globe.controls().enableZoom = false;
+		globe.controls().maxDistance = 200;
+		globe.controls().enableRotate = false;
+	});
+
 </script>
 
 <svelte:head>
@@ -19,7 +54,13 @@
 		description="Check out other chapters of Artify4Kids through this interactive map!"
 		color="bg-a-green"
 	/>
-	<section class="flex h-fit w-full flex-col-reverse bg-a-green-bg xl:flex-row">
+	<div class="bg-a-green-bg flex flex-col items-center">
+		<div bind:this={globeElement} class="z-50 -mt-48"></div>
+	</div>
+	<!-- <section class=" flex flex-col"> -->
+	<!-- </section> -->
+
+	<!-- <section class="flex h-fit w-full flex-col-reverse bg-a-green-bg xl:flex-row">
 		{#if $selectedChapter == 0}
 			<div
 				class="flex h-fit w-full flex-col items-center justify-center bg-white py-14 md:px-14 xl:h-[800px] xl:w-[800px] xl:border-r-2 xl:border-r-a-green-hover/50"
@@ -31,7 +72,7 @@
 						class="mr-2 fill-a-red"
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 576 512"
-						><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+						><path
 							d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"
 						/></svg
 					>
@@ -58,7 +99,7 @@
 						class="mr-2 fill-a-black"
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 384 512"
-						><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+						><path
 							d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"
 						/></svg
 					>
@@ -86,11 +127,11 @@
 			</div>
 		{/if}
 		<Map />
-	</section>
+	</section> -->
 </main>
 
-<style>
+<!-- <style>
 	* {
 		position: relative;
 	}
-</style>
+</style> -->
